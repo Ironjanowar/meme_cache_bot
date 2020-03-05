5f85131397de279f184d492c4cce466ff99fb7ff
Ironjanowar <ironjanowar@gmail.com>
2020-03-05 10:23:57 +0100

    Add gifs

--- lib/meme_cache_bot/utils.ex
+++ lib/meme_cache_bot/utils.ex
@@ -1,9 +1,18 @@
 defmodule MemeCacheBot.Utils do
+  require Logger
+
   def date_now(), do: NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+
+  def get_meme_from_message(%{animation: %{file_id: meme_id, file_unique_id: meme_unique_id}}),
+    do: {:ok, meme_id, meme_unique_id, "gif"}
 
   def get_meme_from_message(%{photo: [%{file_id: meme_id, file_unique_id: meme_unique_id} | _]}),
     do: {:ok, meme_id, meme_unique_id, "photo"}
 
   def get_meme_from_message(%{sticker: %{file_unique_id: meme_unique_id, file_id: meme_id}}),
     do: {:ok, meme_id, meme_unique_id, "sticker"}
+
+  def get_meme_from_message(message) do
+    Logger.debug("Unrecognized meme: #{inspect(message)}")
+  end
 end
