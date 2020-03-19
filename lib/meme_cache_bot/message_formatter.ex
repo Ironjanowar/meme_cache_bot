acdfb42bdf1404ce6782288add9650fa02998449
Ironjanowar <ironjanowar@gmail.com>
2020-03-19 13:26:04 +0100

    Code improvement

--- lib/meme_cache_bot/message_formatter.ex
+++ lib/meme_cache_bot/message_formatter.ex
@@ -8,39 +8,31 @@ defmodule MemeCacheBot.MessageFormatter do
     InlineQueryResultCachedVideo
   }
 
+  defp type_struct("gif"), do: {:ok, InlineQueryResultCachedGif, :gif_file_id, %{}}
+  defp type_struct("photo"), do: {:ok, InlineQueryResultCachedPhoto, :photo_file_id, %{}}
+  defp type_struct("sticker"), do: {:ok, InlineQueryResultCachedSticker, :sticker_file_id, %{}}
+
+  defp type_struct("video"),
+    do: {:ok, InlineQueryResultCachedVideo, :video_file_id, %{title: "Meme"}}
+
+  defp type_struct(invalid_type), do: {:error, :invalid_type, invalid_type}
+
   def get_inline_result(meme) do
-    case meme.meme_type do
-      "gif" ->
-        %InlineQueryResultCachedGif{
-          type: meme.meme_type,
-          id: meme.id,
-          gif_file_id: meme.meme_id
-        }
+    with {:ok, struct_module, file_param, extras} <- type_struct(meme.meme_type) do
+      params =
+        %{type: meme.meme_type, id: meme.id}
+        |> Map.put(file_param, meme.meme_id)
+        |> Map.merge(extras)
 
-      "photo" ->
-        %InlineQueryResultCachedPhoto{
-          type: meme.meme_type,
-          id: meme.id,
-          photo_file_id: meme.meme_id
-        }
+      struct(struct_module, params)
+    else
+      err ->
+        Logger.error("""
+        Invalid meme type #{inspect(meme.meme_type)}:
+          Error: #{inspect(err)}
+          Meme: #{inspect(meme)}
+        """)
 
-      "sticker" ->
-        %InlineQueryResultCachedSticker{
-          type: meme.meme_type,
-          id: meme.id,
-          sticker_file_id: meme.meme_id
-        }
-
-      "video" ->
-        %InlineQueryResultCachedVideo{
-          type: meme.meme_type,
-          id: meme.id,
-          video_file_id: meme.meme_id,
-          title: "Meme"
-        }
-
-      _ ->
-        Logger.debug("Invalid meme type #{inspect(meme.meme_type)}: #{inspect(meme)}")
         :invalid_meme
     end
   end
